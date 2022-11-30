a10c92b2926855c1db70b6e968d96fd8e48fcfbb
faheem4545 <faheemismail4545@yahoo.com>
2022-11-29 21:23:18 -0800

    updated code

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 const ratingBtn = document.querySelectorAll(".btn");
 
+// ratingBtn.forEach((btn) => {
+//   btn.addEventListener("click", (e) => {
+//     const currentValue = e.currentTarget;
+//     ratingBtn.forEach((btn) => {
+//       btn.classList.remove("active");
+//     });
+//     btn.classList.add("active");
+//   });
+//   const submitBtn = document.getElementById("submit");
+//   submitBtn.addEventListener("click", (e) => {
+//     location.href = "thankyou.html";
+//   });
+// });
+
 ratingBtn.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     const currentValue = e.currentTarget;
@@ -7,9 +21,9 @@ ratingBtn.forEach((btn) => {
       btn.classList.remove("active");
     });
     btn.classList.add("active");
-  });
-  const submitBtn = document.getElementById("submit");
-  submitBtn.addEventListener("click", (e) => {
-    location.href = "thankyou.html";
+    const submitBtn = document.getElementById("submit");
+    submitBtn.addEventListener("click", (e) => {
+      location.href = "thankyou.html";
+    });
   });
 });
